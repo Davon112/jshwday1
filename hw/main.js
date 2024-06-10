d71c9f49949d59645c92a9dbf17bf79aeed012bb
Da'Von Manuel <160780050+Davon112@users.noreply.github.com>
2024-06-10 13:19:35 -0500

    Add files via upload

---
+++ hw/main.js
@@ -0,0 +1,45 @@
+let names = "DaVon";
+let ages;
+ages = 29;
+let grades;
+grades = "A"
+let attendance;
+attendance = "Present"
+
+console.log(names, ages, grades, attendance)
+
+let x = 6;
+let y = 2;
+
+let sum = x + y;
+let difference = x-y;
+let product = x*y;
+let quotient = x/y;
+
+
+console.log("Sum:", sum);
+console.log("Difference:", difference);
+console.log("Product:", product);
+console.log("Quotient:", quotient);
+
+x = x + 10;
+y = x * 3;
+console.log("Updated Num1", x)
+console.log("Updated Num2", y)
+
+a = x==y;
+b = x > y;
+c = x < y;
+console.log("Is num1 equal to num2?", a);
+console.log("Is num1 greater than num2?", b);
+console.log("Is num1 not equal to num2?", c);
+
+d = x > 0 && y > 0;
+e = x % 2 == 0 || y % 2 == 0;
+console.log("Are both numbers positive?", d);
+console.log("Is at least one number even?", e);
+
+
+
+
+
